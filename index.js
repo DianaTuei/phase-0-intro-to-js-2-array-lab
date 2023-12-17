596df92694674401fc29667833c70411cf31f060
DianaTuei <memotuei@gmail.com>
2023-12-17 20:32:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop(cats.length-1);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift(0);
+}
+
+function appendCat(name){
+    var copyofCats = cats.slice();
+    copyofCats.push(name);
+    return copyofCats;
+}
+
+function prependCat(name){
+    var copyofCats =cats.slice();
+    copyofCats.unshift(name);
+    return copyofCats;
+}
+
+function removeLastCat(){
+    var copyofCats = cats.slice();
+    copyofCats.pop(name);
+    return copyofCats;
+}
+
+function removeFirstCat(){
+    var copyofCats = cats.slice();
+    copyofCats.shift(name);
+    return copyofCats;
+}
